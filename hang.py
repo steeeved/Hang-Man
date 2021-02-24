646ee44b7c8a7fa9c484dfc408bc29ebd1fd6149
StephenMuchendu <StephenMuchendu@users.noreply.github.com>
2021-02-24 21:27:52 +0300

    first commit

---
+++ hang.py
@@ -0,0 +1,13 @@
+from words import word_list
+import random
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
